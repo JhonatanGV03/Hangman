9e5e2377911735b902c62b69996f5f9e8766ecdd
Jhonatan Gomez <jhonatanva.2003.jsgv@gmail.com>
2022-09-17 21:40:36 -0500

    HangMan

--- src/main/java/com/example/uq/hangman/Main.java
+++ src/main/java/com/example/uq/hangman/Main.java
@@ -3,15 +3,20 @@ package com.example.uq.hangman;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/uq/hangman/img/logo2.png")));
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("game.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 874, 536);
+        stage.getIcons().add(icon);
+        stage.setResizable(false);
         stage.setTitle("HangMan");
         stage.setScene(scene);
         stage.show();
