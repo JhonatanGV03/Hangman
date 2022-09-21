a556665221273120edab29c3b68d6302a14ddfa8
Jhonatan Gomez <jhonatanva.2003.jsgv@gmail.com>
2022-09-20 21:51:27 -0500

    HangMan

--- src/main/java/com/example/uq/hangman/GameController.java
+++ src/main/java/com/example/uq/hangman/GameController.java
@@ -1,6 +1,36 @@
 package com.example.uq.hangman;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
 
 public class GameController {
+
+    private String palabra = "Mia"; //Palabras de prueba
+    private String categoria = "Actriz";
+
+    @FXML
+    private Text txtCategoria;
+
+    @FXML
+    private Text txtPalabra;
+
+    public void initialize (){
+        tamanoPalabra();
+        txtCategoria.setText(categoria);
+    }
+
+    public void tamanoPalabra(){
+        String aux = "";
+        for (int i=0; i<palabra.length(); i++){
+            aux += "_";
+            if (i < palabra.length()-1){
+                aux += "   ";
+            }
+        }
+        System.out.println(aux);
+        txtPalabra.setText(aux);
+    }
+    //Pruebas de relleno de espacio de palabra por descubrir (Esto solo es de prueba más adelante se borran algunas
+    // cosas incluyendo los comentarios que hay)
 
 }
